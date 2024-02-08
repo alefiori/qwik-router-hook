715da6fb35c126702c23238b4634a613eb62088f
Alessandro Fiori <alefiori97@gmail.com>
2024-02-08 22:33:46 +0100

    fix: navigate function

--- src/navigate.ts
+++ src/navigate.ts
@@ -1,8 +1,10 @@
-export const useNavigate = (): { navigate: (_: string) => void } => {
-  const navigate = (route: string) => {
+import { $, QRL } from "@builder.io/qwik";
+
+export const useNavigate = (): { navigate: QRL<(_: string) => void> } => {
+  const navigate = $((route: string) => {
     window.history.pushState({}, "", route);
     dispatchEvent(new PopStateEvent("popstate"));
-  };
+  });
 
   return { navigate };
 };
